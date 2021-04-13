2c180a4630af624a2e2adc949a1d5cfda8be14e7
Meviola Karriqi <82405299+MeviolaKarriqi@users.noreply.github.com>
2021-04-13 19:34:07 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const songs = [
+    {
+        title: "hold on",
+        artist: "Justin Bieber",
+        image: require("../assets/album-arts/hold-on.jpg"),
+        id: "0",
+    },
+    {
+        title: "ghost",
+        artist: "Justin Bieber",
+        image: require("../assets/album-arts/ghost.jpg"),
+        id: "1",
+    },
+    {
+        title: "met him last night",
+        artist: "Demi Lovato ft Ariana Grande",
+        image: require("../assets/album-arts/met-him-last-night.jpg"),
+        id: "2",
+    },
+    {
+        title: "falling",
+        artist: "Harry Styles",
+        image: require("../assets/album-arts/falling.jpg"),
+        id: "3",
+    },
+    {
+        title: "deja vu",
+        artist: "Olivia Rodrigo",
+        image: require("../assets/album-arts/deja-vu.jpg"),
+        id: "4",
+    },
+    {
+        title: "drivers license",
+        artist: "Olivia Rodrigo",
+        image: require("../assets/album-arts/drivers-license.jpg"),
+        id: "5",
+    },
+    {
+        title: "Mr Perfectly Fine",
+        artist: "Taylor Swift",
+        image: require("../assets/album-arts/mr-perfectly-fine.jpg"),
+        id: "6",
+    },
+
+];
+
+export default songs;
